bdbdfa82951d0636bedd1b11e8c47a319d1579bb
Eduardo Cruz <eduardo@eduardocruz.com>
2018-07-06 21:20:21 -0300

    Dummy api url

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/upwork/callback', function (Request $request) {
+	return 'Callback is here';
+});
